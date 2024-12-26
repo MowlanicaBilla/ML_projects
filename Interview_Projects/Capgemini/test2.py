6b94a92e98ac069889d9c5982107d89d71ba632f
MOWLANICA BILLA <39239221+MowlanicaBilla@users.noreply.github.com>
2024-12-26 18:38:11 +0530

    Create test2.py

---
+++ Interview_Projects/Capgemini/test2.py
@@ -0,0 +1,9 @@
+# Accept number from user and check if its palindrome or not
+
+# 121
+
+number = list(str('234'))
+# print(number)
+print(number[0:])
+print(number[-1:])
+# print(number[0:] == number[-1:])
